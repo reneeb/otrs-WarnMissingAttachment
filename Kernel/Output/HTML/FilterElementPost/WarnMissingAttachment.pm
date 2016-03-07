9a059767b2ff890f75dd2a685caac0241adeda76
reneeb <info@perl-services.de>
2016-03-07 11:38:06 +0100

    changes for OTRS5

--- Kernel/Output/HTML/FilterElementPost/WarnMissingAttachment.pm
+++ Kernel/Output/HTML/FilterElementPost/WarnMissingAttachment.pm
@@ -1,20 +1,18 @@
 # --
-# Kernel/Output/HTML/OutputFilterWarnMissingAttachment.pm
-# Copyright (C) 2013 - 2014 Perl-Services.de, http://www.perl-services.de/
+# Kernel/Output/HTML/FilterElementPost/WarnMissingAttachment.pm
+# Copyright (C) 2013 - 2016 Perl-Services.de, http://www.perl-services.de/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
 # did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 # --
 
-package Kernel::Output::HTML::OutputFilterWarnMissingAttachment;
+package Kernel::Output::HTML::FilterElementPost::WarnMissingAttachment;
 
 use strict;
 use warnings;
 
 use List::Util qw(first);
-
-our $VERSION = 0.01;
 
 our @ObjectDependencies = qw(
     Kernel::Config
